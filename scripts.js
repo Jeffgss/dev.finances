e7e5996b2d76c807518174eb0ae9943ff1712a63
Jeff <jefferson.santos@maxmilhas.com.br>
2021-10-26 17:30:33 -0300

    create transactions dynamically

--- scripts.js
+++ scripts.js
@@ -6,3 +6,58 @@ Modal = {
     document.querySelector(".modal-overlay").classList.remove("active");
   },
 };
+
+const transactions = [
+  {
+    id: 1,
+    description: "Luz",
+    amount: -50000,
+    date: "23/01/2021",
+  },
+  {
+    id: 2,
+    description: "Website",
+    amount: 500000,
+    date: "23/01/2021",
+  },
+  {
+    id: 3,
+    description: "Internet",
+    amount: -20000,
+    date: "23/01/2021",
+  },
+];
+
+const Transaction = {
+  incomes() {},
+  expenses() {},
+  total() {},
+};
+
+const DOM = {
+  transactionContainer: document.querySelector("#data-table tbody"),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class=${CSSclass}>${transaction.amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação" />
+      </td>
+    `;
+
+    return html;
+  },
+};
+
+transactions.forEach((transaction) => DOM.addTransaction(transaction));
